bec0547aa5846bba34afc0f96ff9c82e4c291b25
Benaiah-123 <benaiah123456@gmail.com>
2025-08-18 15:51:43 +0800

    initial commit

---
+++ activity9.py
@@ -0,0 +1,15 @@
+a = 3
+b = 7
+c = 10
+d = 3
+ngalan = 'benaiah'
+bantay_bata = 'supersexy'
+
+
+#print(( a < b ) and (a>b))
+#print(( a >= b ) or (a>b))
+#print(( a < b ) or (a>b))
+#print(not(a >= d ) and (c > d))
+#print((a >= d ) and (c > d )
+
+print(( ngalan == 'benaiah' ) and ( bantay_bata == 'supersexy' ))
