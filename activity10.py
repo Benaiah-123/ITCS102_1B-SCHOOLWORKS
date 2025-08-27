096673b23aaedf3e668fa855944092952fac11d8
Benaiah-123 <benaiah123456@gmail.com>
2025-08-27 15:18:38 +0800

    initial commit

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "benaiahescobar"
+password =  "magandaako123"
+
+uname = input ("enter username here -- > ")
+pword = input ("enter password --> ")
+
+if uname == username and pword == password:
+            print ("Access granted")
+else:
+            print ("Acess denied")
