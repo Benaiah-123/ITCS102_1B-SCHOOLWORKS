cf86151929430abb7768e1d04221f43435cdf845
Benaiah-123 <benaiah123456@gmail.com>
2025-08-27 15:18:51 +0800

    initial commit

---
+++ activitysample10.py
@@ -0,0 +1,15 @@
+username = "Benaiah Escobar"
+password = "sanahumabapabuhayko"
+
+uname = input("enter username here -->")
+pword = input("enter your password here --> ")
+
+if uname == username :
+        print ("username correct")
+else:
+        print ("username is not correct")
+
+if pword == password :
+        print ("password correct")
+else:
+        print ("password is not correct")
