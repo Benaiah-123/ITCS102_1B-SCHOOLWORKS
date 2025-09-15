5493705c46ba8b264d162491406d188cd55057ba
Benaiah-123 <benaiah123456@gmail.com>
2025-09-15 15:52:59 +0800

    initial commit

---
+++ codechallenge5.py
@@ -0,0 +1,7 @@
+print ("The Factorial program")
+
+number = eval(input("enter any amount here--> "))
+factorial = 1
+for x in range(number, 0, -1):
+    factorial *= x
+print("the factorial of", number, "is", factorial)
