66f10d9de5ffccd87ec4dfbd29fa47fe0d797da0
Benaiah-123 <benaiah123456@gmail.com>
2025-09-16 11:06:54 +0800

    initial commit

---
+++ code_challenge6.py
@@ -0,0 +1,9 @@
+print("Odd number summation")
+sum = 0
+for y in range (1, 11, 1):
+    number= eval(input("enter any number here--> "))
+    if number % 2:
+        print (y, "is odd")
+        sum += number
+   
+print(sum, "these are the odds!")
