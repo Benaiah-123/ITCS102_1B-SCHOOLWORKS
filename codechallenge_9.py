b529b324d96ac30ae9c97430834edcb83d0a8864
Benaiah-123 <benaiah123456@gmail.com>
2025-09-17 22:39:45 +0800

    codechallenge_9.py

---
+++ codechallenge_9.py
@@ -0,0 +1,6 @@
+print("hi there folk!")
+
+pfft= input("enter any words you want to hear from your parrot? ")
+lols= eval(input("how many times do you want to hear if from your parrot: "))
+for y in range(lols):
+    print("🦜Squawk,",pfft)
