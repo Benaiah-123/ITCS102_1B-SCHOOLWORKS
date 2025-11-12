b3a9fcceab11081787e87c5483da6484537af660
Benaiah-123 <benaiah123456@gmail.com>
2025-11-12 15:52:45 +0800

    Add files via upload

---
+++ activity23.py
@@ -0,0 +1,12 @@
+movie=['powerpop girls','aladdin', 'girl out of the blue']
+print(movie)
+movie.append("miles and the 4 boys")
+print(movie)
+print(movie[3])
+print(movie[1 :3])
+movie.insert(2, "kyri and his pet named miles")
+print(movie)
+movie.remove('aladdin') #will remove aladin only if we use the movie.pop() then it will remove the last part.
+print(movie)
+
+print(len(movie))
